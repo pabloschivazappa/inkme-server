07107356df37abd656488705ce7e14a6cf09b72d
EcaCosca <enriquecoscarelli@gmail.com>
2023-06-07 02:13:23 +0200

    Boiler plate for crud operations

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,4 @@
 const getAllUsers = async (req, res, next) => {
-    res.send("Hello there")
     // try {
     //     const allUsers = await UserCollection.find()
     //     return res.status(200).json(allUsers)
@@ -7,6 +6,28 @@ const getAllUsers = async (req, res, next) => {
     //     next(error)
     // }
 }
+
+const getOneUser = async (req, res, next) => {
+    // try {
+    //     const allUsers = await UserCollection.find()
+    //     return res.status(200).json(allUsers)
+    // } catch (error) {
+    //     next(error)
+    // }
+}
+
+const createOneUser = async (req, res, next) => {
+    
+}
+
+const updateOneUser = async (req, res, next) => {
+
+}
+
+const deleteOneUser = async (req, res, next) => {
+
+}
+
 
 module.exports = {
     getAllUsers
---
+++ models/userModels.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// User schema
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String },
+    bio: { type: String },
+    bookings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Booking' }],
+  });
+  
+//   // Tattoo artist (admin) schema
+//   const tattooArtistSchema = new mongoose.Schema({
+//     username: { type: String, required: true, unique: true },
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true },
+//     profilePicture: { type: String },
+//     bio: { type: String },
+//     services: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Service' }],
+//     bookings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Booking' }],
+//   });
+  
+//   // Service schema
+//   const serviceSchema = new mongoose.Schema({
+//     title: { type: String, required: true },
+//     description: { type: String, required: true },
+//     price: { type: Number, required: true },
+//     artist: { type: mongoose.Schema.Types.ObjectId, ref: 'TattooArtist', required: true },
+//   });
+  
+//   // Booking schema
+//   const bookingSchema = new mongoose.Schema({
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+//     artist: { type: mongoose.Schema.Types.ObjectId, ref: 'TattooArtist', required: true },
+//     service: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: true },
+//     date: { type: Date, required: true },
+//     status: { type: String, enum: ['pending', 'confirmed', 'cancelled'], default: 'pending' },
+//   });
+  
+  const User = mongoose.model('User', userSchema);
+//   const TattooArtist = mongoose.model('TattooArtist', tattooArtistSchema);
+//   const Service = mongoose.model('Service', serviceSchema);
+//   const Booking = mongoose.model('Booking', bookingSchema);
+
+  module.exports = UserCollection
