6b614504030e6059444ff5f143ea0100c9c53b2d
EcaCosca <enriquecoscarelli@gmail.com>
2023-06-07 11:32:49 +0200

    Adding todo's and cleaning code

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,18 @@
 const Router = require('express').Router()
 
 // ALL CONTROLER FUNCTIONS
-const { getAllUsers, getOneUser, createOneUser } = require('../controllers/userControllers')
+const { 
+    getAllUsers,
+    getOneUser,
+    createOneUser
+} = require('../controllers/userControllers')
 
+// ALL ROUTES
 Router.route('/users/all').get(getAllUsers)
-// Router.route('/users/one').get(getOneUser)
 Router.route('/users/createOne').post(createOneUser)
-    
+// TODO
+// Router.route('/users/:id').get(getOneUser)
+// Router.route('/users/:id').put(updateOneUser)
+// Router.route('/users/:id').delete(deleteOneUser)
 
 module.exports = Router
